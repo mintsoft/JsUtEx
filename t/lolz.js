90705b9a78a2b53d4bf163bad6c732b673c4e12c
Rob <rob@mintsoft.net>
2017-04-25 23:24:13 +0100

    Actual sense

--- t/lolz.js
+++ t/lolz.js
@@ -1,28 +1,9 @@
-var vows = require('vows'),
-    assert = require('assert');
+var require('jasmine');
 
 // Create a Test Suite
-vows.describe('Division by Zero').addBatch({
-    'when dividing a number by zero': {
-        topic: function () { return 42 / 0 },
-
-        'we get Infinity': function (topic) {
-            assert.equal (topic, Infinity);
-        }
-    },
-    'but when dividing zero by zero': {
-        topic: function () { return 0 / 0 },
-
-        'we get a value which': {
-            'is not a number': function (topic) {
-                assert.isNaN (topic);
-            },
-            'is not equal to itself': function (topic) {
-                assert.notEqual (topic, topic);
-            },
-	    'is also infinity!': function(topic) {
-		assert.equal(topic, Infinity);
-	    },
-        }
-    }
-}).run(); // Run it
+describe('Division by Zero', function(){
+	var x = 99 /0;
+	it("returns NaN", function(){
+		expect(x).toBe(NaN);
+	});
+});
