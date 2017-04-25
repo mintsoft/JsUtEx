19817d0f14e62e5569237a2f66a6c0893c05b474
Rob <rob@mintsoft.net>
2017-04-25 23:44:32 +0100

    Adding a passing test

--- t/lolz-spec.js
+++ t/lolz-spec.js
@@ -1,6 +1,9 @@
 // Create a Test Suite
 describe('Division by Zero', function(){
 	var x = 99 /0;
+	it("returns Infinity", function(){
+		expect(x).toBe(Infinity);
+	});
 	it("returns NaN", function(){
 		expect(x).toBe(NaN);
 	});
