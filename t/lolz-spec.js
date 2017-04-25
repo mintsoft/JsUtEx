bb15e5e71b2c727bb000c6799f6bd8d2dfdfb2b6
Rob <rob@mintsoft.net>
2017-04-25 23:38:49 +0100

    NaN!

--- t/lolz-spec.js
+++ t/lolz-spec.js
@@ -1,7 +1,10 @@
 // Create a Test Suite
 describe('Division by Zero', function(){
 	var x = 99 /0;
-	it("returns NaN", function(){
+	it("returns Infinity", function(){
 		expect(x).toBe(Infinity);
+	});
+	it("returns NaN", function() {
+		expect(parseFloat("lolz").toBe(NaN);
 	});
 });
